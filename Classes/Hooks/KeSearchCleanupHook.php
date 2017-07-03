5f4cb327798fb55058a80192ce963f381047b180
velletti <jvelletti@allplan.com>
2017-07-03 17:27:26 +0200

    [#147741341] Forum Indexer - Cleanup does not work

--- Classes/Hooks/KeSearchCleanupHook.php
+++ Classes/Hooks/KeSearchCleanupHook.php
@@ -1,29 +1,30 @@
 <?php
 namespace Allplan\AllplanKeSearchExtended\Hooks;
 
-use org\bovigo\vfs\vfsStreamWrapperTestCase;
+// j.v. i think next line  is here by accident !
+// use org\bovigo\vfs\vfsStreamWrapperTestCase;
 
 class KeSearchCleanupHook {
 
-	/**
-	 * Adds indexers to the TCA of the indexer-configuration.
-	 * (You can select these ones in the backend by creating a new record "indexer-configuration")
-	 * @param array $params
-	 * @param mixed $pObj
-	 */
-	public function cleanup(&$where, $pObj) {
-	    $content = "\n In Cleanup Hook: Got $" . "where = " . $where ;
+    /**
+     * Adds indexers to the TCA of the indexer-configuration.
+     * (You can select these ones in the backend by creating a new record "indexer-configuration")
+     * @param array $params
+     * @param mixed $pObj
+     */
+    public function cleanup(&$where, $pObj) {
+        $content = "\n In Cleanup Hook: Got $" . "where = " . $where ;
         switch ($pObj->indexerConfig['type']) {
             case 'onlinehelp' :
                 $where .= " AND `type` = 'allplanhelp' ";
 
                 break ;
             case 'allplanfaq' :
-                $where .= " AND ( `type` = 'allplanfaq' or `type` = 'allplanfaqSP' ) AND tstamp < " . ( time() - ( 60 * 60 * 24 * $pObj->period ) ) ;
+                $where .= " AND ( `type` = 'allplanfaq' or `type` = 'allplanfaqsp' ) AND tstamp < " . ( time() - ( 60 * 60 * 24 * $pObj->period ) ) ;
                 break ;
 
             case 'allplanforum' :
-                $where .= " AND ( `type` LIKE 'allplanforu%' )  AND tstamp < " . ( time() - ( 60 * 60 * 24 * ( $pObj->period ) ) ) ;
+                $where .= " AND ( `type` = 'allplanforum' or `type` = 'allplanforumsp' or `type` = 'allplanforumlocked' )  AND tstamp < " . ( time() - ( 60 * 60 * 24 * ( $pObj->period ) ) ) ;
 
                 break ;
 
@@ -45,7 +46,7 @@ class KeSearchCleanupHook {
             $where .= " AND pid = " . $pObj->indexerConfig['pid'] ;
         }
         return $content . "\n After Cleanup Hook: Now $" . "where = " . $where ;
-	}
+    }
 
 
 
