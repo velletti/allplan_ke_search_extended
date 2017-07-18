5b5275a563a8020c5604e7f4dcda16aa12743103
velletti <jvelletti@allplan.com>
2017-07-18 16:05:55 +0200

    [#147741357] Forum Indexer - indexer for Elearning Videos

--- Classes/Hooks/KeSearchIndexerHook.php
+++ Classes/Hooks/KeSearchIndexerHook.php
@@ -3,33 +3,52 @@ namespace Allplan\AllplanKeSearchExtended\Hooks;
 
 class KeSearchIndexerHook extends BaseKeSearchIndexerHook{
 
-	/**
-	 * Adds indexers to the TCA of the indexer-configuration.
-	 * (You can select these ones in the backend by creating a new record "indexer-configuration")
-	 * @param array $params
-	 * @param mixed $pObj
-	 */
-	public function registerIndexerConfiguration(&$params, $pObj) {
+    /**
+     * Adds indexers to the TCA of the indexer-configuration.
+     * (You can select these ones in the backend by creating a new record "indexer-configuration")
+     * @param array $params
+     * @param mixed $pObj
+     */
+    public function registerIndexerConfiguration(&$params, $pObj) {
 
-		// Events (jv_events)
-		// =================================================================================================================================
-		$newArray = [
-			'Events (jv_events)',
-			'jv_events',
-			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('allplan_ke_search_extended') . '/Resources/Public/Icons/indexer-jv_events.gif'
-		];
-		$params['items'][] = $newArray;
-		unset($newArray);
+        // Events (jv_events)
+        // =================================================================================================================================
+        $newArray = [
+            'Events (jv_events)',
+            'jv_events',
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('allplan_ke_search_extended') . '/Resources/Public/Icons/indexer-jv_events.gif'
+        ];
+        $params['items'][] = $newArray;
+        unset($newArray);
 
-		// Forum
-		// =================================================================================================================================
-		$newArray = [
-			'Allplan Forum ',
-			'allplanforum',
-			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('allplan_ke_search_extended') . '/Resources/Public/Icons/indexer-jv_events.gif'
-		];
-		$params['items'][] = $newArray;
-		unset($newArray);
+        // marit lessons
+        // =================================================================================================================================
+        $newArray = [
+            'Allplan Lessions (marit_elearning)',
+            'lessions',
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('marit_elearning') . '/Resources/Public/Icons/icon_tx_maritelearning_domain_model_lesson.gif'
+        ];
+        $params['items'][] = $newArray;
+        unset($newArray);
+
+        // marit lessons
+        // =================================================================================================================================
+        $newArray = [
+            'Allplan Training Dokumentation (marit_elearning)',
+            'documentation',
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('marit_elearning') . '/Resources/Public/Icons/icon_tx_maritelearning_domain_model_lesson.gif'
+        ];
+        $params['items'][] = $newArray;
+        unset($newArray);
+        // Forum
+        // =================================================================================================================================
+        $newArray = [
+            'Allplan Forum ',
+            'allplanforum',
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('allplan_ke_search_extended') . '/Resources/Public/Icons/indexer-jv_events.gif'
+        ];
+        $params['items'][] = $newArray;
+        unset($newArray);
 
         // Online Help
         // =================================================================================================================================
@@ -61,98 +80,113 @@ class KeSearchIndexerHook extends BaseKeSearchIndexerHook{
         $params['items'][] = $newArray;
         unset($newArray);
 
-		// print_r($GLOBALS['TCA']['tx_kesearch_indexerconfig']['columns']);
+        // print_r($GLOBALS['TCA']['tx_kesearch_indexerconfig']['columns']);
 
-	}
+    }
 
 
-	/**
-	 * Own indexes, writing the data into tx_kesearch_index
-	 * @param array $indexerConfig configuration from TYPO3 backend
-	 * @param \tx_kesearch_indexer $indexerObject reference to the indexer class
-	 * @return  string output in the backend after indexing
-	 */
-	public function customIndexer(&$indexerConfig, &$indexerObject) {
+    /**
+     * Own indexes, writing the data into tx_kesearch_index
+     * @param array $indexerConfig configuration from TYPO3 backend
+     * @param \tx_kesearch_indexer $indexerObject reference to the indexer class
+     * @return  string output in the backend after indexing
+     */
+    public function customIndexer(&$indexerConfig, &$indexerObject) {
 
-		// print_r($indexerConfig);
+        // print_r($indexerConfig);
 
-		$content = '';
+        $content = '';
 
-		// Events (jv_events)
-		// =================================================================================================================================
+        // Events (jv_events)
+        // =================================================================================================================================
 
         switch ($indexerConfig['type']) {
 
             case 'jv_events' :
-                /** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanHelpIndexer $helpIndexer */
-                $helpIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\JvEventsIndexer") ;
+                /** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanHelpIndexer $eventIndexer */
+                $eventIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\JvEventsIndexer");
 
-                $resCount = $helpIndexer->main($indexerConfig, $indexerObject);
+                $resCount = $eventIndexer->main($indexerConfig, $indexerObject);
                 $content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' events where indexed.</p>';
-                break ;
+                break;
 
             case 'allplanforum' :
-                /** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanHelpIndexer $helpIndexer */
-                $helpIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\ForumIndexer") ;
-                $resCount = $helpIndexer->main($indexerConfig, $indexerObject);
+                /** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanHelpIndexer $forumIndexer */
+                $forumIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\ForumIndexer");
+                $resCount = $forumIndexer->main($indexerConfig, $indexerObject);
                 $content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' Forum Entries where indexed.</p>';
-                break ;
+                break;
 
             case 'onlinehelp' :
                 /** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanHelpIndexer $helpIndexer */
-                $helpIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanHelpIndexer") ;
+                $helpIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanHelpIndexer");
 
                 $resCount = $helpIndexer->main($indexerConfig, $indexerObject);
                 $content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' help entries where indexed.</p>';
-                break ;
+                break;
             case 'supportfaq' :
-                /** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanFaqIndexer $helpIndexer */
-                $faqIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanFaqIndexer") ;
+                /** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanFaqIndexer $faqIndexer */
+                $faqIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanFaqIndexer");
 
                 $resCount = $faqIndexer->main($indexerConfig, $indexerObject);
                 $content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' FAQ entries where indexed.</p>';
-                break ;
+                break;
             case 'shop' :
-                /** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanShopIndexer $helpIndexer */
-                $faqIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanShopIndexer") ;
+                /** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanShopIndexer $shopIndexer */
+                $shopIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanShopIndexer");
 
-                $resCount = $faqIndexer->main($indexerConfig, $indexerObject);
+                $resCount = $shopIndexer->main($indexerConfig, $indexerObject);
                 $content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' Shop entries where indexed.</p>';
-                break ;
+                break;
+            case 'lessions' :
+                /** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanShopIndexer $elearningIndexer */
+                $elearningIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanElearningsIndexer");
+
+                $resCount = $elearningIndexer->main($indexerConfig, $indexerObject);
+                $content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' Elearning Video entries where indexed.</p>';
+                break;
+
+            case 'documentation' :
+                /** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanShopIndexer $elearningIndexer */
+                $elearningIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanDocumentationsIndexer");
+
+                $resCount = $elearningIndexer->main($indexerConfig, $indexerObject);
+                $content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' Elearning Documentation entries where indexed.</p>';
+                break;
         }
 
 
-		return $content;
+        return $content;
 
-	}
+    }
 
-	/**
-	 * Modifies the page content fields
-	 * @param $fields
-	 * @param $pObj
-	 */
-	public function modifyPageContentFields(&$fields, $pObj){
+    /**
+     * Modifies the page content fields
+     * @param $fields
+     * @param $pObj
+     */
+    public function modifyPageContentFields(&$fields, $pObj){
 
-		// Add pi_flexform for the allplan content elements
-		$fields.= ',pi_flexform';
+        // Add pi_flexform for the allplan content elements
+        $fields.= ',pi_flexform';
 
-	}
+    }
 
-	/**
-	 * Modifies the content from a content element
-	 * @param $bodytext
-	 * @param $ttContentRow
-	 * @param $pObj
-	 */
-	public function modifyContentFromContentElement(&$bodytext, &$ttContentRow, $pObj){
+    /**
+     * Modifies the content from a content element
+     * @param $bodytext
+     * @param $ttContentRow
+     * @param $pObj
+     */
+    public function modifyContentFromContentElement(&$bodytext, &$ttContentRow, $pObj){
 
-		$additionalContent = $this->getAdditionalContentFromFlexform($ttContentRow);
-		if(!empty($additionalContent)){
-			$additionalContent.='...';
-		}
+        $additionalContent = $this->getAdditionalContentFromFlexform($ttContentRow);
+        if(!empty($additionalContent)){
+            $additionalContent.='...';
+        }
 
-		$bodytext = $additionalContent . $bodytext;
+        $bodytext = $additionalContent . $bodytext;
 
-	}
+    }
 
 }
---
+++ Classes/Utility/AllplanElearningsIndexer.php
@@ -0,0 +1,147 @@
+<?php
+namespace Allplan\AllplanKeSearchExtended\Utility;
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2017 Jörg velletti (allplan) <jVelletti@allplan.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+class AllplanElearningsIndexer extends \Allplan\AllplanKeSearchExtended\Hooks\BaseKeSearchIndexerHook
+{
+    /**
+     * @param array $indexerConfig configuration from TYPO3 backend
+     * @param \tx_kesearch_indexer $indexerObject reference to the indexer class
+     * @return int
+     */
+
+    public function main(&$indexerConfig, &$indexerObject) {
+
+        /**
+         * @var $db \TYPO3\CMS\Core\Database\DatabaseConnection
+         */
+        $db = $GLOBALS['TYPO3_DB'];
+        // $db->store_lastBuiltQuery = true;
+
+        $fields = '*';
+        $table = 'tx_maritelearning_domain_model_lesson as lesson ';
+        $where = \TYPO3\CMS\Backend\Utility\BackendUtility::BEenableFields($table);
+        $where.= \TYPO3\CMS\Backend\Utility\BackendUtility::deleteClause($table);
+
+        // echo "Select " . $fields . " FROM " . $table . " WHERE " . $where ;
+        // die;
+
+        $res = $db->exec_SELECTquery($fields,$table,$where);
+        $resCount = $db->sql_num_rows($res);
+
+        // echo "ResCount: " . $resCount . "<hr>" ;
+        $origData = array() ;
+
+        if($resCount) {
+            $resCount = 0 ;
+
+            while(( $record = $db->sql_fetch_assoc($res))) {
+                $resCount++ ;
+                // Prepare data for the indexer
+
+                $title = $record['title'] ;
+
+                $abstract = $record['title'] ;
+                $description = $record['title'] ;
+
+                $content = $title . PHP_EOL . nl2br($abstract) . PHP_EOL . nl2br($description);
+
+
+                $tags = '#videos#';
+                $sys_language_uid = $record['sys_language_uid'] ;
+
+                /** @var \DateTime $sortdate */
+                $sortdate = $record['date']   ;
+                $endtime = 0 ;
+                $feGroup = '';
+                $debugOnly = false;
+
+                $parameters = [
+                    'tx_marit_elearning[download]=' . intval( $record['uid'] ),
+                    'tx_marit_elearning[action]=show',
+                    'tx_marit_elearning[controller]=Download'
+                ];
+                $origId = $record['l18n_parent'] ;
+                $feGroup = $record['fe_group'] ;
+                unset($recordOrig) ;
+                if( $origId > 0 ) {
+                    if( is_array( $origData[$origId] )) {
+                        $recordOrig = $origData[$origId] ;
+                    } else {
+
+                        $resSingle = $db->exec_SELECTquery($fields,$table,"uid = " . $origId );
+                        $recordOrig = $db->sql_fetch_assoc($resSingle) ;
+                        $origData[$origId]  = $recordOrig ;
+                    }
+                    $feGroup = $recordOrig['fe_group'] ;
+                }
+                // echo "<br>" . $resCount . " LocalizedUid() " . $record['uid']  . " Parent Uid:" . $record['l18n_parent'] . " LNG: " . $sys_language_uid . " orig: " . $recordOrig['uid'];
+                // echo " fe Group : " . $feGroup ;
+
+                $feGroupArray = explode("," , $feGroup ) ;
+                if( in_array("1" , $feGroupArray ) || in_array("3" , $feGroupArray ) || in_array("10" , $feGroupArray ) || in_array("11" , $feGroupArray ) || in_array("8" , $feGroupArray ) ) {
+                    // if video is available for everybody ( forum member) or normal Customers we will include this in search index.
+                    // The Final Access is managed by the extension. So only Intarmal videos or Videos just for students will an fe Groups entry
+                    $feGroup = '' ;
+                }
+                // echo " -> " . $feGroup . " |";
+
+
+                // The following should be filled (in accordance with the documentation), see also:
+                // http://www.typo3-macher.de/facettierte-suche-ke-search/dokumentation/ein-eigener-indexer/
+                $additionalFields = array(
+                    'orig_uid' => $record['uid']  ,
+                    'sortdate' => 0  ,
+                    'servername' => $_SERVER['SERVER_NAME']
+                );
+                if( $sortdate > 0 )  {
+                    $additionalFields['sortdate'] = intval( $sortdate )  ;
+                   //  $additionalFields['sortdate'] = $sortdate->getTimestamp() ;
+                }
+
+
+                $indexerObject->storeInIndex(
+                    $indexerConfig['pid'],			// folder, where the indexer Data is stored
+                    $title,							// title in the result list
+                    'lessons',				    // content type Important
+                    $indexerConfig['targetpid'],	// uid of the targetpage (see indexer-config in the backend)
+                    $content, 						// below the title in the result list
+                    $tags,							// tags (not used here)
+                    '&' . implode('&', $parameters),						// additional typolink-parameters, e.g. '&tx_jvevents_events[event]=' . $record['uid'];
+                    $abstract,						// abstract (not used here)
+                    $sys_language_uid,				// sys_language_uid
+                    0 ,						// starttime (not used here)
+                    $endtime,						// endtime (not used here)
+                    $feGroup,						// fe_group (not used here)
+                    $debugOnly,						// debug only?
+                    $additionalFields				// additional fields added by hooks
+                );
+            }
+
+        }
+        // echo "ResCount: " . $resCount . "<hr>" ;
+        // die;
+        return intval($resCount);
+    }
+}
