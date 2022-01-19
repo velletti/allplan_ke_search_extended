3521d99df2cd067e68da9ad55185637d0e470501
pbenke <pbenke@allplan.com>
2022-01-19 15:36:45 +0100

    TYPO3-769 #comment Refactoring CustomIndexerHook

--- Classes/Hooks/CustomIndexerHook.php
+++ Classes/Hooks/CustomIndexerHook.php
@@ -1,6 +1,28 @@
 <?php
 namespace Allplan\AllplanKeSearchExtended\Hooks;
 
+/**
+ * AllplanKeSearchExtended
+ */
+use Allplan\AllplanKeSearchExtended\Indexer\AllplanContentserveIndexer;
+use Allplan\AllplanKeSearchExtended\Indexer\AllplanDocumentationsIndexer;
+use Allplan\AllplanKeSearchExtended\Indexer\AllplanElearningsIndexer;
+use Allplan\AllplanKeSearchExtended\Indexer\AllplanFaqIndexer;
+use Allplan\AllplanKeSearchExtended\Indexer\AllplanHelpIndexer;
+use Allplan\AllplanKeSearchExtended\Indexer\AllplanKesearchIndexer;
+use Allplan\AllplanKeSearchExtended\Indexer\AllplanShopIndexer;
+use Allplan\AllplanKeSearchExtended\Indexer\ForumIndexer;
+use Allplan\AllplanKeSearchExtended\Indexer\JvEventsIndexer;
+
+/**
+ * Doctrine
+ */
+use Doctrine\DBAL\Driver\Exception as DoctrineDBALDriverException;
+
+/**
+ * GeneralUtility
+ */
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class CustomIndexerHook
 {
@@ -8,79 +30,69 @@ class CustomIndexerHook
 	/**
 	 * Own indexes, writing the data into tx_kesearch_index
 	 * @param array $indexerConfig configuration from TYPO3 backend
-	 * @param \Tpwd\KeSearch\Indexer\IndexerRunner $indexerObject reference to the indexer class
-	 * @return  string output in the backend after indexing
+	 * @param AllplanKesearchIndexer $indexerObject reference to the indexer class
+	 * @return string output in the backend after indexing
+	 * @throws DoctrineDBALDriverException
 	 * @author Jörg Velletti <jvelletti@allplan.com>
 	 * @author Peter Benke <pbenke@allplan.com>
 	 */
-	public function customIndexer(&$indexerConfig, &$indexerObject) {
-
-		// print_r($indexerConfig);
+	public function customIndexer(array &$indexerConfig, AllplanKesearchIndexer &$indexerObject): string
+	{
 
 		$content = '';
 
 		switch ($indexerConfig['type']){
 
 			case 'jv_events':
-				/** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanHelpIndexer $eventIndexer */
-				$eventIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\JvEventsIndexer");
-
+				$eventIndexer = GeneralUtility::makeInstance(JvEventsIndexer::class);
 				$resCount = $eventIndexer->main($indexerConfig, $indexerObject);
 				$content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' events where indexed.</p>';
 				break;
 
-			case 'allplanforum' :
-				/** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanHelpIndexer $forumIndexer */
-				$forumIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\ForumIndexer");
+			case 'allplanforum':
+				$forumIndexer = GeneralUtility::makeInstance(ForumIndexer::class);
 				$resCount = $forumIndexer->main($indexerConfig, $indexerObject);
-				$content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' Forum Entries where indexed.</p>';
+				$content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' forum Entries where indexed.</p>';
 				break;
 
-			case 'onlinehelp' :
-				/** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanHelpIndexer $helpIndexer */
-				$helpIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanHelpIndexer");
-
+			case 'onlinehelp':
+				$helpIndexer = GeneralUtility::makeInstance(AllplanHelpIndexer::class);
 				$resCount = $helpIndexer->main($indexerConfig, $indexerObject);
-				$content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' help entries where indexed.</p>';
+				$content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' Allplan help entries where indexed.</p>';
 				break;
-			case 'supportfaq' :
-				/** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanFaqIndexer $faqIndexer */
-				$faqIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanFaqIndexer");
 
+			// Todo: Check first, if we are on Connect
+			case 'supportfaq':
+				$faqIndexer = GeneralUtility::makeInstance(AllplanFaqIndexer::class);
 				$resCount = $faqIndexer->main($indexerConfig, $indexerObject);
 				$content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' FAQ entries where indexed.</p>';
 				break;
-			case 'shop' :
-				/** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanShopIndexer $shopIndexer */
-				$shopIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanShopIndexer");
 
+			case 'shop':
+				$shopIndexer = GeneralUtility::makeInstance(AllplanShopIndexer::class);
 				$resCount = $shopIndexer->main($indexerConfig, $indexerObject);
 				$content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' Shop entries where indexed.</p>';
 				break;
-			case 'lessions' :
-				/** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanShopIndexer $elearningIndexer */
-				$elearningIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanElearningsIndexer");
 
+			// Todo check spelling
+			case 'lessions':
+				$elearningIndexer = GeneralUtility::makeInstance(AllplanElearningsIndexer::class);
 				$resCount = $elearningIndexer->main($indexerConfig, $indexerObject);
 				$content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' Elearning Video entries where indexed.</p>';
 				break;
 
-			case 'documentation' :
-				/** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanShopIndexer $elearningIndexer */
-				$elearningIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanDocumentationsIndexer");
-
+			case 'documentation':
+				$elearningIndexer = GeneralUtility::makeInstance(AllplanDocumentationsIndexer::class);
 				$resCount = $elearningIndexer->main($indexerConfig, $indexerObject);
 				$content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' Elearning Documentation entries where indexed.</p>';
 				break;
-			case 'contentserve' :
-				/** @var \Allplan\AllplanKeSearchExtended\Utility\AllplanShopIndexer $elearningIndexer */
-				$contentIndexer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance("Allplan\\AllplanKeSearchExtended\\Utility\\AllplanContentserveIndexer");
 
+			case 'contentserve':
+				$contentIndexer = GeneralUtility::makeInstance(AllplanContentserveIndexer::class);
 				$resCount = $contentIndexer->main($indexerConfig, $indexerObject);
-				$content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' Contentserve  entries where indexed.</p>';
+				$content = '<p><strong>Indexer "' . $indexerConfig['title'] . '"</strong>:<br>' . $resCount . ' Contentserve entries where indexed.</p>';
 				break;
 		}
-
 
 		return $content;
 
