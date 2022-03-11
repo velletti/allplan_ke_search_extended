4b1bf80faf1789a732c24ca1d3e6bc1d4a4c4e3a
pbenke <pbenke@allplan.com>
2022-03-11 09:03:38 +0100

    TYPO3-769 #comment Clean CleanupHook

--- Classes/Hooks/CleanupHook.php
+++ Classes/Hooks/CleanupHook.php
@@ -4,7 +4,6 @@ namespace Allplan\AllplanKeSearchExtended\Hooks;
 /**
  * AllplanKeSearchExtended
  */
-
 use Allplan\AllplanKeSearchExtended\Utility\DbUtility;
 use Allplan\AllplanKeSearchExtended\Utility\EnvironmentUtility;
 use Allplan\AllplanKeSearchExtended\Utility\FormatUtility;
@@ -24,14 +23,11 @@ class CleanupHook
 	 * Called in ke_search/Classes/Indexer/IndexerRunner.php->cleanUpIndex()
 	 * -----------------------------------------------------------------------------------------------------------------
 	 * Deletes all index elements that are older than starting timestamp in registry (+ conditions from us)
-	 * Todo: => Into documentation
 	 * Explanation:
 	 * - At first ke_search runs an indexer and inserts / updates index entries in table tx_kesearch_index
 	 *   On start ke_search writes a tstamp in sys_registry with data 'tx_kesearch' => 'startTimeOfIndexer'
 	 * - After the index process is finished, the ke_search->cleanUpIndex() function is called, which calls this hook here
 	 *   ke_search deletes all index entries, where the tstamp is older than the 'startTimeOfIndexer' in sys_registry
-	 * => This is good for indexers, which do a full index everytime, but not e.g. for forum, where we have an indexing
-	 *    process step by step because of the big number of records
 	 * -----------------------------------------------------------------------------------------------------------------
 	 * Normally all records would be deleted by ke_search
 	 * Added conditions here:
@@ -100,102 +96,6 @@ class CleanupHook
 
 		// Used in TYPO3 backend / email (CLI mode)
 		return $content;
-
-
-		# allplan_online_help
-
-/*
-		$content = "\n Before CleanupHook: Got $" . "where = " . $where;
-
-		switch ($pObj->indexerConfig['type']){
-
-
-			case 'onlinehelp':
-				$where .= " AND `type` = 'allplanhelp' ";
-				if($pObj->storagePid){
-					$where .= " AND pid = " . $pObj->storagePid;
-				} else {
-					$where .= " AND pid = " . $pObj->indexerConfig['pid'];
-				}
-				if(isset($pObj->language[0]) && $pObj->language[0] != ''){
-					$where .= " AND language = " . intval( $pObj->language[0]);
-				}
-				break;
-
-			// Todo
-			case 'supportfaq':
-				$where .= " AND ( `type` = 'supportfaq' or `type` = 'supportfaq' ) AND tstamp < " . (time() - (60 * 60 * 24 * $pObj->period));
-				if($pObj->storagePid){
-					$where .= " AND pid = " . $pObj->storagePid;
-				}else{
-					$where .= " AND pid = " . $pObj->indexerConfig['pid'];
-				}
-				if (isset($pObj->language[0]) && $pObj->language[0] != '') {
-					$where .= " AND language = " . intval( $pObj->language[0]);
-				}
-				break;
-
-			// Todo
-			case 'lessons':
-				$where .= " AND ( `type` = 'lessons' or `type` = 'lessonslocked' ) AND tstamp < " . (time() - (60 * 60 * 24 * ($pObj->period)));
-				break;
-
-			// Todo
-			case 'documentations':
-				$where .= " AND ( `type` = 'documentation' or `type` = 'documentationlocked' ) AND tstamp < " . (time() - (60 * 60 * 24 * ($pObj->period)));
-				break ;
-
-			// Todo
-			case 'allplanforum':
-				$where .= " AND (`type` = 'allplanforum' OR `type` = 'allplanforumsp' OR `type` = 'allplanforumlocked') AND tstamp < " . (time() - (60 * 60 * 24 * ($pObj->period)));
-				break;
-
-
-			// Todo
-			default:
-				$where .= " AND `type` = '" . $pObj->indexerConfig['type'] .  "' ";
-				if($pObj->storagePid){
-					$where .= " AND pid = " . $pObj->storagePid;
-				} else {
-					$where .= " AND pid = " . $pObj->indexerConfig['pid'];
-				}
-				if(is_array( $pObj->language ) && $pObj->language[0] != '' ){
-					$where .= " AND language = " . intval( $pObj->language[0]);
-				}
-				break;
-
-		}
-
-		// Todo: change query
-		$where .= " AND ( servername ='" . EnvironmentUtility::getServerName() . "' OR servername = '' ) ";
-		return $content . "\n After CleanupHook: Now $" . "where = " . $where;
-*/
-	}
-
-	private function getDeleteConditionsDefault($indexerRunner, $indexerConfig)
-	{
-		$deleteConditions = [];
-
-		// pid
-		$deleteConditions[] = "`pid` = " . (int)IndexerUtility::getStoragePid($indexerRunner, $indexerConfig);
-
-		// type
-		$deleteConditions[] = "`type` = '" . $indexerConfig['type'] . "'";
-
-		// consider language only, if explicit set
-		try{
-			$language = IndexerUtility::getLanguage($indexerRunner);
-		}catch(Exception $e){
-			$language  = null;
-		}
-		if(!is_null($language) && $language != ''){
-			$deleteConditions[] = "`language` = '" . $language . "'";
-		}
-
-	}
-
-	private function getDeleteConditionsIndexForum()
-	{
 
 	}
 
