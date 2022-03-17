ae0d5aafd50b149cbdb9cbf80f735b87323b74ad
pbenke <pbenke@allplan.com>
2022-03-17 15:45:32 +0100

    TYPO3-769 #comment Rewrite ModifySearchWordsHook

--- Classes/Hooks/ModifySearchWordsHook.php
+++ Classes/Hooks/ModifySearchWordsHook.php
@@ -15,22 +15,21 @@ class ModifySearchWordsHook
 {
 
 	/**
-	 * Modify search input => sets default to AND-search (+)
-	 * (you can select these in the backend by creating a new record "indexer-configuration")
+	 * - If GET parameter 'directory' is set (Allplan faq) => urldecode 'wordsAgainst' and 'sword'
+	 * - If search string is at least 3 characters and contains '+' or ' ' => build AND conditions for MATCH AGAINST
 	 * @param array $searchWordInformation
 	 * @param Pluginbase $pObj
-	 * @author Jörg Velletti <jvelletti@allplan.com>
 	 * @author Peter Benke <pbenke@allplan.com>
 	 */
 	public function modifySearchWords(array &$searchWordInformation, Pluginbase &$pObj)
 	{
-
-		// Todo: check this
-
+		# GeneralUtility::_GP()
 		$searchWordInformation['wordsAgainst'] = trim($searchWordInformation['wordsAgainst']);
+		// print_r(['start' => $searchWordInformation]);
 
-		// Set directory in search input
+		// If GET parameter 'directory' is set (Allplan faq) => urldecode 'wordsAgainst' and 'sword'
 		if(isset($_GET['tx_kesearch_pi1']['directory'])){
+
 			if ($searchWordInformation['wordsAgainst'] == '') {
 				$searchWordInformation['wordsAgainst'] = urldecode(trim($_GET['tx_kesearch_pi1']['directory']));
 			}
@@ -39,31 +38,51 @@ class ModifySearchWordsHook
 			}
 		}
 
-		if(strlen($searchWordInformation['wordsAgainst']) > 2){
-			$searchWordInformation['wordsAgainst'] = str_replace("+", "", $searchWordInformation['wordsAgainst']);
+		// If search string is at least 3 characters and contains '+' or ' ' => build AND conditions for MATCH AGAINST
+		$wordsAgainst = trim($searchWordInformation['wordsAgainst']);
+		if(
+			strlen($wordsAgainst) > 2
+			&&
+			(
+				preg_match('/ /', $wordsAgainst)
+				||
+				preg_match('/\+/', $wordsAgainst)
+			)
+		){
 
-			// @extensionScannerIgnoreLine
-			// set user input in search field as html_encoded(), if defined in typoscript
-			if($pObj->conf['encodeWords']){
-				$searchWordInformation['wordsAgainst'] = htmlentities($searchWordInformation['wordsAgainst']);
+			// print_r(['wordsAgainst before' => $searchWordInformation]);
+
+			/**
+			 * If we have a '+' or ' ' in search string => build an AND-condition for 'MATCH ... AGAINST'
+			 * Example: 'searchString1+searchString2' or 'searchString1 + searchString2' (with spaces) or 'searchString1 searchString2'
+			 * => should end in:
+			 *
+			 * MATCH (title,content) AGAINST ('+searchString1* +searchString2*' IN BOOLEAN MODE)...
+			 *
+			 * @see https://dev.mysql.com/doc/refman/8.0/en/fulltext-boolean.html
+			 * + stands for AND
+			 * - stands for NOT
+			 * [no operator] implies OR
+			 */
+
+			$wordsAgainst = trim($searchWordInformation['wordsAgainst']);
+			$wordsAgainst = str_replace('+',' ', $wordsAgainst);
+			$wordsAgainst = str_replace('*',' ', $wordsAgainst); // might already come from ke_search
+			$wordsAgainst = trim($wordsAgainst);
+
+			$wordsAgainst = preg_replace('#\s+#', ' ', $wordsAgainst); // multiple spaces to one space
+			$wordsAgainst = explode(' ', $wordsAgainst);
+			$matchesAgainst = [];
+
+			// Now build the new 'wordsAgainst'
+			foreach($wordsAgainst as $wordAgainst){
+				$matchesAgainst[] = '+' . $wordAgainst . '*';
 			}
-			$arr = GeneralUtility::trimExplode(" ", $searchWordInformation['wordsAgainst']);
-			if ($arr && count($arr) > 3){
-				$fixed = "";
-				foreach ($arr as $key => $sword){
-					if($key > 2){
-						$sword = str_replace("*","", $sword);
-					}
-					$fixed .= " " . $sword;
-				}
-				$searchWordInformation['wordsAgainst'] = trim($fixed);
 
-			}
+			// Set 'wordsAgainst' to the new value
+			$searchWordInformation['wordsAgainst'] = implode(' ', $matchesAgainst);
 
-			// var_dump($searchWordInformation['wordsAgainst']);
-			// die(" __FILE__" . __FILE__ . " __LINE__" . __LINE__ );
-
-			$searchWordInformation['wordsAgainst'] = trim("+" . str_replace(" "," +", $searchWordInformation['wordsAgainst']));
+			// print_r(['wordsAgainst afterwards' => $searchWordInformation]);
 
 		}
 
