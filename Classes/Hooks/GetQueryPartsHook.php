93e8b3cd45d16ccdb3910bfb1f0d87a0356bcc99
pbenke <pbenke@allplan.com>
2022-03-17 13:25:13 +0100

    TYPO3-769 #comment Cleanup code

--- Classes/Hooks/GetQueryPartsHook.php
+++ Classes/Hooks/GetQueryPartsHook.php
@@ -33,62 +33,67 @@ class GetQueryPartsHook
 	 */
 	public function getQueryParts(array $queryParts , $pibase , $searchwordQuoted=null)
 	{
-		// Todo check this
 
+		$debug = [];
 
 		/*
-		 * see connect TYPO Script file f.e.:
+		 * see connect typoscript file e.g.:
 		 * http/typo3conf/ext/connect_template/Configuration/TypoScript/Base/Setup/Extensions/ke_search_for_support.ts
-		 * will add conditions f.e. type = "supportfaq%" or set defaultSearch = top10 > 0
+		 * will add conditions e.g. type = "supportfaq%" or set defaultSearch = top10 > 0
 		 */
-		$debug[] = "line: " . __LINE__  . " init";
+		$debug[] = 'Line: ' . __LINE__  . ' init';
 
 		// @extensionScannerIgnoreLine
 		$resultPage = $pibase->conf['resultPage'];
 
 		$settings = TyposcriptUtility::loadTypoScriptFromScratch($resultPage,'tx_kesearch_pi1');
-		$debug[] = "line: " . __LINE__  . " queryParts is array";
+		$debug[] = 'Line: ' . __LINE__  . ' queryParts is array';
 
-		$hookData = null;
+		$tsConfigQueryParts = null;
 
 		// If typoscript is set (Allplan faq)
-		if(is_array($settings) && array_key_exists("getQueryPartsHook", $settings)){
+		if(is_array($settings) && array_key_exists('getQueryPartsHook', $settings)){
 
-			$debug[] = "line: " . __LINE__ . " found getQueryPartsHook in settings";
-			$hookData = $settings['getQueryPartsHook'];
+			$debug[] = 'Line: ' . __LINE__ . ' "getQueryPartsHook" is configured in typoscript';
+			$tsConfigQueryParts = $settings['getQueryPartsHook'];
 
-			// If we have a WHERE-condition, consider top10
-			if(is_array($hookData) && array_key_exists("where", $hookData)) {
-				$debug[] = "line: " . __LINE__ . " hookData is array and has WHERE-condition";
-				$queryParts['WHERE'] .= " AND ( " ;
-				foreach ($hookData['where'] as $query){
+			// If we have a WHERE-condition in typoscript set => consider top10
+			if(is_array($tsConfigQueryParts) && array_key_exists('where', $tsConfigQueryParts)) {
+
+				$debug[] = 'Line: ' . __LINE__ . ' hookData is array and has WHERE-condition';
+				$queryParts['WHERE'].= ' AND ( ';
+
+				foreach($tsConfigQueryParts['where'] as $query){
 					$queryParts['WHERE'] .= " (`" . trim($query['field']) . "` " . trim($query['type']) . " '" . trim( $query['value'] ) . "') ";
 				}
+
 				$queryParts['WHERE'] .= " ) ";
-				$queryParts['SELECT'] = str_replace( ") AS score" , " + (top10)) AS score" , $queryParts['SELECT']);
+				$queryParts['SELECT'] = str_replace( ") AS score"," + (top10)) AS score" , $queryParts['SELECT']);
+
 			}
 
-			if(is_array($hookData) && array_key_exists("defaultSearch", $hookData)){
+			// If we have "defaultSearch" in typoscript set
+			if(is_array($tsConfigQueryParts) && array_key_exists('defaultSearch', $tsConfigQueryParts)){
 
-				$debug[] = "line: " . __LINE__ . " searchDefault is configured";
-				$debug[] = "line: " . __LINE__ . " queryParts['WHERE'] = " . $queryParts['WHERE'];
+				$debug[] = 'Line: ' . __LINE__ . ' "defaultSearch" is configured in typoscript';
+				$debug[] = 'Line: ' . __LINE__ . " queryParts['WHERE'] = " . $queryParts['WHERE'];
 
-				// check if we have an empty search-word, and we are not filtering by directory
+				// Check, if we have an empty search-word, and we are not filtering by directory
 				if(
 					strpos($queryParts['WHERE'],"MATCH (title,content) AGAINST ('+' IN BOOLEAN MODE) ") > 0
 					||
 					strpos($queryParts['WHERE'],"MATCH (title,content) AGAINST ") < 1
 				) {
-					$debug[] = "line: " . __LINE__ . " queryParts['WHERE'] matches empty search";
-					$debug[] = "line: " . __LINE__ . " match if a) " . strpos($queryParts['WHERE'],"MATCH (title,content) AGAINST ('+' IN BOOLEAN MODE) ");
-					$debug[] = "line: " . __LINE__ . " match if b) " . strpos($queryParts['WHERE'],"MATCH (title,content) AGAINST ");
+					$debug[] = 'Line: ' . __LINE__ . " queryParts['WHERE'] matches empty search";
+					$debug[] = 'Line: ' . __LINE__ . " match if a) " . strpos($queryParts['WHERE'],"MATCH (title,content) AGAINST ('+' IN BOOLEAN MODE) ");
+					$debug[] = 'Line: ' . __LINE__ . " match if b) " . strpos($queryParts['WHERE'],"MATCH (title,content) AGAINST ");
 
 					if(strpos($queryParts['WHERE']," `directory`") < 1){
 						$queryParts['WHERE'] = str_replace("MATCH (title,content) AGAINST ('+' IN BOOLEAN MODE) ","", $queryParts['WHERE']) . " AND ( ";
 					} else {
 						$queryParts['WHERE'] = $queryParts['WHERE'] . " ( ";
 					}
-					foreach($hookData['defaultSearch'] as $query){
+					foreach($tsConfigQueryParts['defaultSearch'] as $query){
 						$queryParts['WHERE'] .= " ( `" . trim($query['field']) . "` " . trim($query['type']);
 						if(trim($query['type']) == ">" || trim($query['type']) == "<"){
 							$queryParts['WHERE'] .= " " . trim($query['value']) . " ) ";
@@ -101,38 +106,49 @@ class GetQueryPartsHook
 				}
 			}
 
-			$debug[] = "line: " . __LINE__ ;
 			$queryParts['SELECT'] = str_replace( ") AS score"," + (top10)) AS score", $queryParts['SELECT']);
+			$debug[] = 'Line: ' . __LINE__ . " queryParts['SELECT'] = " . $queryParts['SELECT'];
+
 		}
 
-		$params = GeneralUtility::_GET("tx_kesearch_pi1");
-		if(is_array($params) && array_key_exists("directory", $params) && strlen($params["directory"]) > 0 ){
-			$debug[] = "line: " . __LINE__ . " got parameter *directory*";
-			if(strpos($params["directory"],";") < 1 && strpos($params["directory"],")") < 1 && strpos($params["directory"],"'") < 1){
+		$params = GeneralUtility::_GET('tx_kesearch_pi1');
+
+		// If parameter 'directory' is given as GET parameter (from faq select box)
+		if(is_array($params) && array_key_exists('directory', $params) && strlen($params['directory']) > 0 ){
+
+			$debug[] = 'Line: ' . __LINE__ . " GET parameter *directory* given";
+
+			if(strpos($params['directory'],';') < 1 && strpos($params['directory'],')') < 1 && strpos($params['directory'],"'") < 1){
 				$databaseConnection = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('tx_kesearch_index');
-				$directory = trim(substr(urldecode($params["directory"]),0,-1)) . "%";
-				$debug[] = "line: " . __LINE__ . " parameter *directory* is: " . $directory;
+				$directory = trim(substr(urldecode($params['directory']),0,-1)) . '%';
+				$debug[] = 'Line: ' . __LINE__ . " GET parameter *directory* is: " . $directory;
 				$directoryQuoted = $databaseConnection->quote(
 					str_replace( "\\" , "\\\\", $directory),
 					PDO::PARAM_STR
 				);
-				$debug[] = "line: " . __LINE__ . " parameter *directory* is now: " . $directoryQuoted;
-				$queryParts['WHERE'] .= " AND ( directory LIKE " . $directoryQuoted . " ) ";
+				$debug[] = 'Line: ' . __LINE__ . " parameter *directory* is now: " . $directoryQuoted;
+				$queryParts['WHERE'] .= ' AND ( directory LIKE ' . $directoryQuoted . ' ) ';
 			}
 		}
 		$queryParts['WHERE'] = str_replace("MATCH (title,content) AGAINST ","MATCH (title,content,directory) AGAINST ", $queryParts['WHERE']);
 
 		// Output debug
 		if(1==2){
-			echo "<hr><pre>Hook Data:";
-			print_r($hookData);
-			echo "<hr>" ;
-			print_r($debug);
-			echo "<hr>" ;
-			print_r($queryParts);
-			die("__FILE__" . __FILE__ . " __LINE__" . __LINE__);
+
+			$debugOutput = '<hr><pre>Typoscript config query parts:';
+			$debugOutput.= print_r($tsConfigQueryParts, true);
+			$debugOutput.= '<hr>';
+			$debugOutput.= print_r($debug, true);
+			$debugOutput.= '<hr>';
+			$debugOutput.= print_r($queryParts, true);
+
+			// echo $debugOutput; die("__FILE__" . __FILE__ . " __LINE__" . __LINE__);
+			mail('pbenke@allplan.com', 'Faq debug output', print_r(['Debug' => $debugOutput], true));
+
 		}
+
 		return $queryParts;
+
 	}
 
 }
